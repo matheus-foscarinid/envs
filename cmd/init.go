51c5c10e1fcb454875cabb67ab291f5986647aa0
Matheus Foscarini Dias <matheus.foscarinid@gmail.com>
2026-02-09 21:15:00 -0300

    feat: add iniitial list + init commands

--- cmd/init.go
+++ cmd/init.go
@@ -4,8 +4,12 @@ Copyright © 2026 Matheus Foscarini Dias <matheus.foscarinid@gmail.com>
 package cmd
 
 import (
+	"encoding/json"
 	"fmt"
+	"os"
 
+	"github.com/matheus-foscarinid/envs/internal/config"
+	"github.com/matheus-foscarinid/envs/internal/constants"
 	"github.com/spf13/cobra"
 )
 
@@ -15,13 +19,59 @@ var initCmd = &cobra.Command{
 	Short: "Initialize a new environment",
 	// TODO: Add a longer description
 	Long: `Initialize a new environment with a sample file and a config file.
-		Creates a new .env.sample that will be the template for the new environment, if it doesn't exist.
 		Creates a new .env.json that will be the config file for envs manager.
+		Creates a new .env.sample that will be the template for the new environment, if it doesn't exist.
 		Creates a new .env file based on the sample file, if it doesn't exist.
 	`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("init called")
+		fmt.Println("⛰️ Initializing envs...")
+		fmt.Println("\n")
+
+		// check if the config file exists
+		if _, err := os.Stat(constants.ConfigFile); os.IsNotExist(err) {
+			fmt.Println("Creating default config file...")
+			createDefaultConfigFile()
+		} else {
+			fmt.Println("✅ Config file already exists, skipping...")
+		}
+
+		// check if the sample file exists
+		if _, err := os.Stat(constants.SampleFile); os.IsNotExist(err) {
+			fmt.Println("Creating default sample file...")
+			os.Create(constants.SampleFile)
+		} else {
+			fmt.Println("✅ Sample file already exists, skipping...")
+		}
+
+		// create the env file
+		if _, err := os.Stat(constants.DotEnvFile); os.IsNotExist(err) {
+			fmt.Println("Creating default env file...")
+			os.Create(constants.DotEnvFile)
+		} else {
+			fmt.Println("✅ Env file already exists, skipping...")
+		}
+
+		fmt.Println("\n")
+		fmt.Println("⛰️ Envs initialized successfully!")
 	},
+}
+
+func createDefaultConfigFile() error {
+	file, err := os.Create(constants.ConfigFile)
+	if err != nil {
+		fmt.Println("Error creating config file:", err)
+		return err
+	}
+	defer file.Close()
+
+	// write the default config file
+	config := config.Config{
+		Version: 1,
+		Active:  "default",
+	}
+
+	json.NewEncoder(file).Encode(config)
+	return nil
 }
 
 func init() {
--- internal/manager/manager.go
+++ internal/manager/manager.go
@@ -1,8 +1,54 @@
 package manager
 
-import "github.com/matheus-foscarinid/envs/internal/config"
+import (
+	"fmt"
+	"path/filepath"
+
+	"github.com/matheus-foscarinid/envs/internal/config"
+	"github.com/matheus-foscarinid/envs/internal/constants"
+)
 
 type Manager struct {
 	cfg *config.Config
 }
 
+func currentEnv() (string, error) {
+	// get the active env from the config file
+	cfg, err := config.Load(constants.ConfigFile)
+	if err != nil {
+		return "", err
+	}
+	return cfg.Active, nil
+}
+
+func ListEnvs() ([]string, error) {
+	pattern := ".env.*"
+	files, err := filepath.Glob(pattern)
+	filteredFiles := []string{}
+	for _, file := range files {
+		if file == constants.SampleFile {
+			continue
+		}
+		filteredFiles = append(filteredFiles, file)
+	}
+
+	currentEnv, err := currentEnv()
+	if err != nil {
+		return nil, err
+	}
+
+	if len(filteredFiles) == 0 {
+		fmt.Println("No envs found")
+		return nil, nil
+	}
+
+	for _, file := range filteredFiles {
+		if file == currentEnv {
+			fmt.Println("*", file)
+		} else {
+			fmt.Println(file)
+		}
+	}
+
+	return nil, nil
+}
