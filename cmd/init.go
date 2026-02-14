a13b623a8b60bbf2aa597bcaf42aed469f830b6c
Matheus Foscarini Dias <matheus.foscarinid@gmail.com>
2026-02-14 14:23:04 -0300

    chore: cleanup bolierplate comments

--- cmd/init.go
+++ cmd/init.go
@@ -1,6 +1,3 @@
-/*
-Copyright © 2026 Matheus Foscarini Dias <matheus.foscarinid@gmail.com>
-*/
 package cmd
 
 import (
@@ -10,7 +7,6 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// initCmd represents the init command
 var initCmd = &cobra.Command{
 	Use:   "init",
 	Short: "Initialize a new environment",
@@ -29,14 +25,4 @@ var initCmd = &cobra.Command{
 
 func init() {
 	rootCmd.AddCommand(initCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// initCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// initCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
